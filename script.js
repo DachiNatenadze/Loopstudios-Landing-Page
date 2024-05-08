fd9542c9169b757726aa7e7ca0e8563d267ff582
Dachi Natenadze <dachinatenadzeb@gmail.com>
2024-05-08 16:50:11 -0700

    Responsive dekstop done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let burger = document.querySelector("burgerActive");
+let burger = document.querySelector(".burgerMenu");
+let burgerActive = document.querySelector(".burgerActive");
+burger.addEventListener("click", () => {
+  burgerActive.classList.toggle("burgerActive");
+});
+let close = document.querySelector(".x");
+close.addEventListener("click", () => {
+  burgerActive.classList.toggle("burgerActive");
+});
